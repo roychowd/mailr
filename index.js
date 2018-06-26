6b7acef4eab6a17c035dc331b74fa4a49511f9ce
roychowd <konishky@ualberta.ca>
2018-06-25 23:11:59 -0600

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: "There" })
+    res.send({hi: "Thedsfre" })
 });
 
 const PORT = process.env.PORT || 5000;
