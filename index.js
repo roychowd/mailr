8513ee70fbdfe578c1fd789740abec328f1e3797
roychowd <konishky@ualberta.ca>
2018-06-28 23:30:37 -0600

    authentication almost compelete

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const mongoose = require('mongoose');
-require('./services/passport'); 
-const keys = require('./config/keys');
-
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/user"); // for db
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-require('./routes/authroutes')(app);
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authroutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
